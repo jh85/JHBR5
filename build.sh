43b757ddd08d407cb1090ec3bcedd7be3ff24c2b
ei <1856754+jh85@users.noreply.github.com>
2026-07-28 09:00:00 -0400

    Use epoch-3 opt-64 TensorRT build defaults

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 export CUDA_PATH=/usr/local/cuda
 export TENSORRT_PATH=/data/new_jhbr2/TensorRT-11.1.0.106
 export CUDNN_PATH=/data/new_jhbr2/cudnn-linux-x86_64-9.24.0.43_cuda13-archive
-export MODEL_ONNX=/data/new_jhbr2/JHBR3/shogi_bt4_epoch1_dynamic_fp16.onnx
+export MODEL_ONNX=/data/new_jhbr2/JHBR3/shogi_bt4_epoch3_dynamic_fp16.onnx
 export CC=clang
 export CXX=clang++
 export BUILD_DIR=build-trt
+export BSIZE=64
+export PSIZE=16384
 
 export LD_LIBRARY_PATH=$TENSORRT_PATH/lib:$CUDNN_PATH/lib:$CUDA_PATH/lib64:$LD_LIBRARY_PATH
 
@@ -28,8 +30,8 @@ mkdir -p engines
 
 $TENSORRT_PATH/bin/trtexec \
   --onnx="$MODEL_ONNX" \
-  --saveEngine=engines/shogi_bt4_epoch23_trt_o128_m128_ws8192.engine \
+  --saveEngine=engines/shogi_bt4_epoch3_trt_o"$BSIZE"_m"$BSIZE"_ws"$PSIZE".engine \
   --minShapes=input_planes:1x148x9x9 \
-  --optShapes=input_planes:128x148x9x9 \
-  --maxShapes=input_planes:128x148x9x9 \
-  --memPoolSize=workspace:8192M
+  --optShapes=input_planes:"$BSIZE"x148x9x9 \
+  --maxShapes=input_planes:"$BSIZE"x148x9x9 \
+  --memPoolSize=workspace:"$PSIZE"M
